efa32fc604a4861f3670dce670b969a1d93a00ce
ThomasCote <tcote19@gmail.com>
2021-06-02 17:46:55 -0700

    Initial Commit

---
+++ IMOVED/IMOVEDApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMOVEDApp.swift
+//  IMOVED
+//
+//  Created by Thomas Cote on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct IMOVEDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
